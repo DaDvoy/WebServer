8e524a01a8d4d24f9c5ae0c73d1ec07707a48cb0
DaDvoy <lmushroo@student.21-school.ru>
2022-01-31 15:21:54 +0300

    fix main

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char  *argv[])
         }
         std::cout << "\n\n+++++++ Ending request parser ++++++++\n";
 
-        write(new_socket , hello_there , strlen(hello_there));
+        write(new_socket , hello_there  , strlen(hello_there));
         printf("------------------Response sent-------------------\n");
         close(new_socket);
     }
