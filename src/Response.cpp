a1df7a960a8fe2c9c9d435e52a681c01debea4c1
DaDvoy <lmushroo@student.21-school.ru>
2022-03-03 19:16:42 +0300

    for updates

--- src/Response.cpp
+++ src/Response.cpp
@@ -14,6 +14,6 @@ Response::~Response()
 
 void        Response::buildResponse() {
     std::string firstLine(requestParser.request.query.protocol + status.getCode());
-    std::map<std::string, std::string>::iterator it_beg = this->headers.begin();
+//    std::map<std::string, std::string>::iterator it_beg = this->headers.begin();
 
 }
--- src/response/headlines.cpp
+++ src/response/headlines.cpp
@@ -3,10 +3,13 @@
 void            headlines::searchKey() {
     RequestParser requestParser;
 
-    contentType = requestParser.request.head["Accept"] + "/r/n";
-    contentEncoding = requestParser.request.head["Accept-encoding"] + "/r/n"
+    if (requestParser.request.head["Accept-Encoding"] != requestParser.request.head.end())
+        contentEncoding = requestParser.request.head["Accept-Encoding"] + "/r/n" // really?
     if (contentLenght = requestParser.request.body) // if the request has body
-        intLenght = contentLenght.size();
-    mb_
+        intLenght = contentLenght.size(); //todo: How I need to count lenght?
+    contentType = requestParser.request.head["Accept"] + "/r/n";
 
+
+    //
+    // todo: server: gulu gulu
 }
