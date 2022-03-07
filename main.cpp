67ae82eb819f4eb74bbc59a5e5b9faa94d30a8c1
fcarl <totallabyss@gmail.com>
2022-03-07 17:40:18 +0300

    main check

--- main.cpp
+++ main.cpp
@@ -25,11 +25,10 @@ int main(int argc, char  *argv[], char *env[])
 	std::cout << "client_max_body_size: " << config->client_max_body_size << std::endl;
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     int server_fd, new_socket;
-    struct sockaddr_in address;
+    sockaddr_in address;
     int addrlen = sizeof(address);
     
-    std::string hello = "Hello from server";
-    char *hello_there;
+    std::string hello_there = "Hello from server";
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("In socket");
@@ -42,7 +41,7 @@ int main(int argc, char  *argv[], char *env[])
 
     memset(address.sin_zero, '\0', sizeof address.sin_zero);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    if (bind(server_fd, (sockaddr *)&address, sizeof(address))<0)
     {
         perror("In bind");
         exit(EXIT_FAILURE);
@@ -55,7 +54,7 @@ int main(int argc, char  *argv[], char *env[])
     while(1)
     {
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        if ((new_socket = accept(server_fd, (sockaddr *)&address, (socklen_t*)&addrlen))<0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
@@ -67,29 +66,32 @@ int main(int argc, char  *argv[], char *env[])
         
         std::cout << "\n+++++++ Query string ++++++++\n\n";
 
-        std::cout << "query_string: " << requestParser.request.query.query_string << std::endl;
-        std::cout << "method: " << requestParser.request.query.method << std::endl;
-        std::cout << requestParser.request.query.address << std::endl;
-        std::cout << requestParser.request.query.protocol << std::endl << std::endl;
+        Request newRequest = requestParser.request; //так можно присвоить новый реквест объекту реквест для удобства
+
+        std::cout << "query_string: " << newRequest.query.query_string << std::endl;
+        std::cout << "method: " << newRequest.query.method << std::endl;
+        std::cout << newRequest.query.address << std::endl;
+        std::cout << newRequest.query.protocol << std::endl << std::endl;
 
         std::cout << "\n+++++++ HEADERS ++++++++\n\n";
 
-        std::map<std::string, std::string>::iterator it = requestParser.request.head.begin();
-        std::map<std::string, std::string>::iterator it_end = requestParser.request.head.end();
+        std::map<std::string, std::string>::iterator it = newRequest.head.begin(); //для итерации по мапе заголовков нужно использовать итераторы
+        std::map<std::string, std::string>::iterator it_end = newRequest.head.end();
 
-        
+        std::cout << "\n+++++++ BODY ++++++++\n\n";
+        std::cout << newRequest.body << std::endl;        
         while (it != it_end) // вывод заголовков
         {
-            // std::cout << it->first << ": " <<  "|" << it->second << "|" << std::endl;
+            std::cout << it->first << ": " << it->second << std::endl;
             it++;
         }
         std::cout << "\n\n+++++++ Ending request parser ++++++++\n";
-
-        CommonGatewayInterface *cgi = new CommonGatewayInterface("cgi/test.cgi", env);
+        (void)env;
+        //CommonGatewayInterface *cgi = new CommonGatewayInterface("cgi/test.cgi", env, newRequest, address, *config);
         //cgi->ExecuteCGI(new_socket);
-
-        hello_there = (char *)cgi->ExecuteCGI(new_socket).c_str();
-        write(new_socket, hello_there, strlen(hello_there));
+        //std::cout << "host: " << newRequest.head["Host"] << std::endl;
+        // hello_there = (char *)cgi->ExecuteCGI().c_str();
+        write(new_socket, hello_there.c_str(), strlen(hello_there.c_str()));
         printf("------------------Response sent-------------------\n");
         close(new_socket);
     }
